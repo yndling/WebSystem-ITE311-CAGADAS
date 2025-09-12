3bfd256cea1ee91dabe3761f9575c809abda5b88
yndling <jccagadas12@gmail.com>
2025-09-12 17:59:52 +0800

    update for defualt page

--- app/Views/template.php
+++ app/Views/template.php
@@ -6,9 +6,7 @@
     <title>Bootstrap Template</title>
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate, max-age=0" />
-    <meta http-equiv="Pragma" content="no-cache" />
-    <meta http-equiv="Expires" content="Sat, 01 Jan 2000 00:00:00 GMT" />
+    
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
